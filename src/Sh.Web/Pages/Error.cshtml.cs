9830ac3d0f5b996ffbdf2e1750856994f96f2839
Futuricon <keepline@inbox.ru>
2021-07-13 06:37:47 +0500

    Localization & SEO improved, also fixed some bugs

--- src/Sh.Web/Pages/Error.cshtml.cs
+++ src/Sh.Web/Pages/Error.cshtml.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Sh.Web.Pages
 {
@@ -17,15 +13,13 @@ namespace Sh.Web.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
+        public ErrorModel()
         {
-            _logger = logger;
         }
 
         public void OnGet()
         {
+            ViewData["PageName"] = "ErrorPage";
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
--- src/Sh.Web/Pages/Privacy.cshtml.cs
+++ src/Sh.Web/Pages/Privacy.cshtml.cs
@@ -1,24 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Sh.Web.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel()
         {
-            _logger = logger;
         }
 
         public void OnGet()
         {
+            ViewData["PageName"] = "PrivacyPage";
         }
     }
 }
